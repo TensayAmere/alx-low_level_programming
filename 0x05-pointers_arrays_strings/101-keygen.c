23b8b30b46d569a0aebfd7ba7ab6a448dba20d3e
TensayAmere <101726216+TensayAmere@users.noreply.github.com>
2022-04-19 01:45:48 +0300

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,12 +1,28 @@
+#include "main.h"
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <time.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - check the code for Holberton School students.
+ * 
+ * Return: Always 0.
  */
 int main(void)
 {
+char seed[53] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char password[22];
+int i, n, passw, complem;
 
-	return (0);
+srand(time(0));
+
+for (i = 0; passw < 2772; i++)
+{
+n = rand() % 10;
+password[i] = seed[n];
+passw += password[i];
+}
+complem = 2772 - passw;
+password[i] = complem;
+printf("%s\n",  password);
+return (0);
 }
