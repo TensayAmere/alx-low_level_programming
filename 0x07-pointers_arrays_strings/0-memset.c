f2b7e386ea00f63c215438245ccb7dfb72eb8a71
TensayAmere <101726216+TensayAmere@users.noreply.github.com>
2022-04-27 21:11:16 +0300

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,21 +1,20 @@
 #include "main.h"
 
 /**
- * memset - Fills the first n bytes of the memory area
- *          pointed to by @s with the constant byte @c.
- * @s: A pointer to the memory area to be filled.
- * @c: The character to fill the memory area with.
- * @n: The number of bytes to be filled.
+ * _memset - fills memory with constant byte
+ * @s: memory area
+ * @b: constant byte b
+ * @n: first n bytes of memory area pointed by s
  *
- * Return: A pointer to the filled memory area @s.
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-void *_memset(void *s, int c, size_t n)
+char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int index;
-	unsigned char *memory = s, value = c;
+	unsigned int i;
 
-	for (index = 0; index < n; index++)
-		memory[index] = value;
-
-	return (memory);
+	for (i = 0; i < n; i++, s++)
+		*s = b;
+	s = s - n;
+	return (s);
 }
